28a3459bdc58e9130b6424c5e69334cbe3f1c6ad
Vitor Rodrigues <42361382+vitor-mauricio@users.noreply.github.com>
2022-02-01 17:36:56 -0300

    Update PingAction.java

--- src/main/java/com/appsecco/dvja/controllers/PingAction.java
+++ src/main/java/com/appsecco/dvja/controllers/PingAction.java
@@ -42,7 +42,7 @@ public class PingAction extends BaseController {
 
     private void doExecCommand() throws IOException {
         Runtime runtime = Runtime.getRuntime();
-        String[] command = { "/bin/bash", "-c", "ping -t 5 -c 5 " };
+        String[] command = { "/bin/bash", "-c", "ping -t 5 -c 5 " + getAddress() };
         Process process = runtime.exec(command);
 
         BufferedReader  stdinputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
